aba6f8ad759dc6bd355b5435622a3dac7acc5841
tiann <923551233@qq.com>
2016-04-07 14:13:56 +0800

    rollback

--- classloader-hook/src/main/java/com/weishu/upf/hook_classloader/MainActivity.java
+++ classloader-hook/src/main/java/com/weishu/upf/hook_classloader/MainActivity.java
@@ -1,12 +1,11 @@
 package com.weishu.upf.hook_classloader;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,10 +23,8 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
 
-    // patch宿主ClassLoader的方式
     private static final int PATCH_BASE_CLASS_LOADER = 1;
 
-    // 自定义ClassLoader的方式
     private static final int CUSTOM_CLASS_LOADER = 2;
 
     private static final int HOOK_METHOD = CUSTOM_CLASS_LOADER;
@@ -45,50 +42,20 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 try {
-                    //                    Intent t = new Intent();
-                    //                    if (HOOK_METHOD == PATCH_BASE_CLASS_LOADER) {
-                    //                        t.setComponent(new ComponentName("com.weishu.upf.dynamic_proxy_hook.app2",
-                    //                                "com.weishu.upf.dynamic_proxy_hook.app2.MainActivity"));
-                    //                    } else {
-                    //                        t.setComponent(new ComponentName("com.weishu.upf.ams_pms_hook.app",
-                    //                                "com.weishu.upf.ams_pms_hook.app.MainActivity"));
-                    //                    }
-                    //                    startActivity(t);
-
-                    Uri uri = Uri.fromFile(getFileStreamPath("test.apk"));
-                    boolean ret = (Boolean) invokeMethod(Class.forName("android.provider.DocumentsContract").newInstance(), "isDocumentUri", new Class[] { Context.class, Uri.class },
-                            new Object[] { getApplicationContext(), uri });
-                    Log.d(TAG, "ret" + ret);
+                    Intent t = new Intent();
+                    if (HOOK_METHOD == PATCH_BASE_CLASS_LOADER) {
+                        t.setComponent(new ComponentName("com.weishu.upf.dynamic_proxy_hook.app2",
+                                "com.weishu.upf.dynamic_proxy_hook.app2.MainActivity"));
+                    } else {
+                        t.setComponent(new ComponentName("com.weishu.upf.ams_pms_hook.app",
+                                "com.weishu.upf.ams_pms_hook.app.MainActivity"));
+                    }
+                    startActivity(t);
                 } catch (Throwable e) {
                     e.printStackTrace();
                 }
             }
         });
-    }
-
-    private static Method getDeclaredMethod(Object object, String name, Class<?>[] types) {
-        for (Class<?> superClass = object.getClass(); superClass != Object.class; superClass.getSuperclass()) {
-            try {
-                return superClass.getDeclaredMethod(name, types);
-            } catch (NoSuchMethodException e) {
-
-            }
-        }
-        return null;
-    }
-
-    private static Object invokeMethod(Object obj, String methodName, Class<?>[] argTypes, Object[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method = getDeclaredMethod(obj, methodName, argTypes);
-        if (method == null) {
-            throw new IllegalAccessException("could not found method:" + methodName);
-        }
-        method.setAccessible(true);
-        try {
-            return method.invoke(obj, args);
-        } catch (IllegalAccessException e) {
-
-        }
-        return null;
     }
 
     @Override
